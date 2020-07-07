1e146300957591dfaf06ea7af45fe87b94797a6d
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2020-07-07 17:14:10 -0300

    calculator project

---
+++ calculator/lib/components/display.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Display extends StatelessWidget {
+  final String text;
+  Display(this.text);
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Color.fromRGBO(48, 48, 48, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(text),
+          ],
+        ),
+      ),
+    );
+  }
+}
