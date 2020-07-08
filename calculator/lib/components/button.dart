7b9f8867bba08b4b181424bf677bea5abab8ac36
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2020-07-08 16:15:12 -0300

    keyboard style and constructors

--- calculator/lib/components/button.dart
+++ calculator/lib/components/button.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
+  // variaveis de button
+  static const DARK = Color.fromRGBO(82, 82, 82, 1);
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
   final String text;
+  final bool big;
+  final color;
+
+  // construtor
   Button({
     @required this.text,
+    this.big = false,
+    this.color = DEFAULT,
   });
+  Button.big({
+    @required this.text,
+    this.big = true,
+    this.color = DEFAULT,
+  });
+  Button.operation({
+    @required this.text,
+    this.big = false,
+    this.color = OPERATION,
+  });
+
+  // widget
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      flex: 1,
+      flex: big ? 2 : 1,
       child: RaisedButton(
-        child: Text(text),
+        color: this.color,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.w200,
+          ),
+        ),
         onPressed: () {},
       ),
     );
