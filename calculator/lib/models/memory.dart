08c9dd2aa2200dede1f7ad2b86902930c6d25e27
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2020-07-13 15:29:57 -0300

    digit 0 and operations basics

--- calculator/lib/models/memory.dart
+++ calculator/lib/models/memory.dart
@@ -2,14 +2,33 @@
 // atraves de cliques em keyboard e responsavel por mostrar os dados
 // no display
 class Memory {
+  static const operations = const ['%', '/', 'x', '-', '+', '='];
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  String operation;
   String _value = '0';
+  bool _wipeValue = false;
 
   void applyCommand(String command) {
     if (command == 'AC') {
       _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
     } else {
-      _value += command;
+      _addDigit(command);
     }
+  }
+
+  _setOperation(String newOperation) {
+    _wipeValue = true;
+  }
+
+  _addDigit(String digit) {
+    final wipeValue = _value == '0' || _wipeValue;
+    final currentValue = wipeValue ? '' : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+    // _value += digit;
   }
 
   _allClear() {
